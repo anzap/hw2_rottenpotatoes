ab0e358b61336faf489ad9e1ad19cee1c26f30ad
saasbook <saasbook@saasbook.(none)>
2012-03-12 14:11:51 -0700

    commiting sorting functionality for movies based on title and release date

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if !params[:orderby].nil?
+      if params[:orderby] == "title"
+        @movies = Movie.order("title") 
+      elsif params[:orderby] == "release_date"
+        @movies = Movie.order("release_date")
+      end
+    else
+      @movies = Movie.all    
+    end
   end
 
   def new
